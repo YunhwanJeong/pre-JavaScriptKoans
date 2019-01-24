50fd54c2fa097b66cd8c3cfaeedd3b38f435993c
Lee Hoyong <igotoweb@gmail.com>
2019-01-24 19:27:08 +0900

    remove trail whitespace rename advanced koans

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -1,26 +1,26 @@
 describe("About Functions", function() {
 
   it("should declare functions", function() {
-    
+
     function add(a, b) {
       return a + b;
     }
-    
+
     expect(add(1, 2)).toBe(FILL_ME_IN);
   });
 
   it("should know internal variables override outer variables", function () {
     var message = "Outer";
-    
+
     function getMessage() {
       return message;
     }
-    
+
     function overrideMessage() {
       var message = "Inner";
       return message;
     }
-    
+
     expect(getMessage()).toBe(FILL_ME_IN);
     expect(overrideMessage()).toBe(FILL_ME_IN);
     expect(message).toBe(FILL_ME_IN);
@@ -39,33 +39,33 @@ describe("About Functions", function() {
   });
 
   it("should use lexical scoping to synthesise functions", function () {
-    
+
     function makeIncreaseByFunction(increaseByAmount) {
       return function (numberToIncrease) {
         return numberToIncrease + increaseByAmount;
       };
     }
-    
+
     var increaseBy3 = makeIncreaseByFunction(3);
     var increaseBy5 = makeIncreaseByFunction(5);
-    
+
     expect(increaseBy3(10) + increaseBy5(10)).toBe(FILL_ME_IN);
   });
 
   it("should allow extra function arguments", function () {
-    
+
     function returnFirstArg(firstArg) {
       return firstArg;
     }
-    
+
     expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
-    
+
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
     }
-    
+
     expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
-    
+
     function returnAllArgs() {
       var argsArray = [];
       for (var i = 0; i < arguments.length; i += 1) {
@@ -73,7 +73,7 @@ describe("About Functions", function() {
       }
       return argsArray.join(",");
     }
-    
+
     expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
   });
 
@@ -82,17 +82,17 @@ describe("About Functions", function() {
     var appendRules = function (name) {
       return name + " rules!";
     };
-    
+
     var appendDoubleRules = function (name) {
       return name + " totally rules!";
     };
-    
+
     var praiseSinger = { givePraise: appendRules };
     expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
-    
+
     praiseSinger.givePraise = appendDoubleRules;
     expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
-      
+
   });
-     
+
 });

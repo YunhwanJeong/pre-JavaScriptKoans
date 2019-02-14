2f43e3811a715fd90f4d8713af558b9de953ecf8
Lee Hoyong <igotoweb@gmail.com>
2019-02-14 14:02:32 +0900

    add npm task

---
+++ test/index.js
@@ -0,0 +1,19 @@
+global.FILL_ME_IN = "Fill this value in";
+
+describe('Bare Minimum Requirements', function() {
+  require('../koans/AboutExpects.js');
+  require('../koans/AboutArrays.js');
+  require('../koans/AboutFunctions.js');
+  require('../koans/AboutObjects.js');
+});
+/*
+xdescribe('Advanced Content', function() {
+  require('../koans/Advanced-AboutHigherOrderFunctions.js');
+  require('../koans/Advanced-AboutMutability.js');
+});
+
+xdescribe('Advanced Content', function() {
+  require('../koans/Advanced-AboutInheritance.js');
+  require('../koans/Advanced-AboutApplyingWhatWeHaveLearnt.js');
+});
+*/

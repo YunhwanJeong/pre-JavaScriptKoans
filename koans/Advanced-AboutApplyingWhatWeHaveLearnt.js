235201e50e7c00af16eae3578284a3dc545509ef
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-07-12 17:28:46 +0900

    SYNC

--- koans/Advanced-AboutApplyingWhatWeHaveLearnt.js
+++ koans/Advanced-AboutApplyingWhatWeHaveLearnt.js
@@ -32,14 +32,21 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+      /* solve using filter() & all() / any() */
 
       var productsICanEat = [];
+      var noNutsProducts = products.filter(function (product) {
+        return product.containsNuts === false;
+      })
+      var hasMushrooms = noNutsProducts.ingredients.some(function(ing) {
+        return ing === "mushrooms";
+      })
 
-      /* solve using filter() & all() / any() */
+
 
       expect(productsICanEat.length).toBe(FILL_ME_IN);
   });
